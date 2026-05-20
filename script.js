878cff77fb159999c78411419883f0e341be2a0d
architraj99 <rajarchit99@gmail.com>
2026-05-20 20:59:32 +0530

    Made basic word scramble game UI

---
+++ script.js
@@ -0,0 +1,13 @@
+let wordBox = document.querySelector(".word-box");
+
+let guessInput = document.getElementById("guessInput");
+
+let checkBtn = document.getElementById("checkBtn");
+
+let nextBtn = document.getElementById("nextBtn");
+
+let message = document.querySelector(".message");
+
+let score = document.querySelector(".score");
+
+let lives = document.querySelector(".lives");
